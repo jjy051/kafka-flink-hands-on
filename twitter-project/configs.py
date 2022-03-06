9c35e4ef7957339bd618315d598be0ab12b96009
Jaeyoung Jang <dmos7777@gmail.com>
2022-03-06 22:38:50 +0900

    tt

--- twitter-project/configs.py
+++
@@ -1,6 +0,0 @@
-### keep the api key info in secret.
-### set this file on git ignore file.
-consumer_key="dVXMTzyp6GgSmjY2wr6yxLhDG"
-consumer_secret="me8thQ7tl9mzXS2cLmwEKnoeZ3MPn7JVDLSJifgyRHnrhJPpxj"
-access_token="1500341992533860353-7MeBfahEsQicmzteciVj6Sn7bqZOpf"
-access_token_secret="5Q1nex1URCxyWqR8T49NcFC3zCunGVC41mqtxqSYDkIh4"
